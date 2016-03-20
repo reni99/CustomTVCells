4823683408a5c742b94d0f642427fa5d1a3c50a1
reni99 <renato.stauffer90@googlemail.com>
2016-03-20 10:11:53 +0100

    Importing project

---
+++ CustomTVCells/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTVCells
+//
+//  Created by Renato Stauffer on 20/03/16.
+//  Copyright © 2016 App2Tap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
